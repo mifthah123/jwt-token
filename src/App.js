1b3cf5c0545770283110f720826d9d2f70c22f0a
mifthah123 <mifthahmuhammed792@gmail.com>
2022-09-19 20:25:41 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import Admin from "./Admin";
+import Users from "./Users";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Users /> },
+    { path: "/admin", element: <Admin /> },
+  ]);
+  return routes;
+};
+
+const AppWrapper = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <App />
+    </Router>
   );
-}
+};
 
-export default App;
+export default AppWrapper;
